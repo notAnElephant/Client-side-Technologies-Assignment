7659c59781b3e715b024067c6f843879aa381dea
Oliver Raisz <raiszoliver@gmail.com>
2023-06-03 18:46:12 +0200

    Add project files.

---
+++ Clientside-Technologies-Assignment/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Clientside_Technologies_Assignment;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ Clientside-Technologies-Assignment/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Clientside_Technologies_Assignment;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
